d19010fb4f56c0c8b7d5d31dfd42b14701e7053d
Dadoright <donabusi1@gmail.com>
2022-01-20 21:02:32 +0530

    Changes are suggested

--- portfolio/src/Pages/Intro.js
+++ portfolio/src/Pages/Intro.js
@@ -5,6 +5,7 @@ function Intro() {
     <div>
       <h3>Ayubowan</h3>
       <p>Hi My Name is Denuka Jayaweera and I am from Kurunegala Srilanka</p>
+      {/* Adjust all the angles and stuff color is not good looking  */}
     </div>
   );
 }
